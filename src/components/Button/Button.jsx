4990fecc127f5c384e6fca2b9f3b9e7bde32cd3a
husni47 <husnithecitizen@gmail.com>
2023-11-25 18:02:30 +0700

    Update button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,15 @@
 /* eslint-disable react/prop-types */
-const Button = ({ title}) => {
-    return (
-        <div className="py-3 px-6 bg-DARKBLUE05 text-white text-center rounded-2xl mt-2 text-sm">
-            {title}
-        </div>
-    )
-}
+const Button = ({ title }) => {
+  return (
+    // <div className="py-3 px-6 bg-DARKBLUE05 text-white text-center rounded-2xl mt-2 text-sm">
+    //   {title}
+    // </div>
 
-export default Button
+    //update button responsive
+    <button className="w-50 pt-3 pr-40 pb-3 pl-40 bg-DARKBLUE05 text-white rounded-xl">
+      {title}
+    </button>
+  );
+};
+
+export default Button;
