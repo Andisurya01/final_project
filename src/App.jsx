1ccf5c4b22c24ce8a095a06faa55a6ad3152fd4d
husni47 <husnithecitizen@gmail.com>
2023-11-28 02:01:12 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from '../src/pages/Login'
-import Register from '../src/pages/Register'
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "../src/pages/Login";
+import Register from "../src/pages/Register";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/Login' element={<Login />}></Route>
-          <Route path='/Register' element={<Register />}></Route>
+          <Route path="/Login" element={<Login />}></Route>
+          <Route path="/Register" element={<Register />}></Route>
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
