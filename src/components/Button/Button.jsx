33913b97d763a4804b58c9545886e05ed7600e68
husni47 <husnithecitizen@gmail.com>
2023-11-28 00:31:42 +0700

    Fix bug in login functionality

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,18 +1,9 @@
 /* eslint-disable react/prop-types */
 const Button = ({ title }) => {
   return (
-    // <div className="py-3 px-6 bg-DARKBLUE05 text-white text-center rounded-2xl mt-2 text-sm">
-    //   {title}
-    // </div>
-
-    //update button responsive
-    <button
-      class="w-THREESEVEN pt-2.5 pb-2.5 pl-3 rounded-xl border-2 bg-DARKBLUE05 text-white text-sm"
-      title={title}
-      onClick={onClick}
-    >
+    <div className="py-3 px-6 bg-DARKBLUE05 text-white text-center rounded-2xl mt-2 text-sm">
       {title}
-    </button>
+    </div>
   );
 };
 
