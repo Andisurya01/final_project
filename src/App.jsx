4872bcd57d7691f15ae8ded9a9505ce5c2c181a6
husni47 <husnithecitizen@gmail.com>
2023-11-29 06:00:09 +0700

    Create Page ResetPassword

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "../src/pages/Login";
 import Register from "../src/pages/Register";
+import ResetTautan from "./pages/ResetTautan";
+import Reset from "./pages/Reset";
 function App() {
   return (
     <>
@@ -9,6 +11,8 @@ function App() {
         <Routes>
           <Route path="/Login" element={<Login />}></Route>
           <Route path="/Register" element={<Register />}></Route>
+          <Route path="/ResetTautan" element={<ResetTautan />}></Route>
+          <Route path="/Reset" element={<Reset />}></Route>
         </Routes>
       </BrowserRouter>
     </>
