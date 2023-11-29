c9d20fce3324229752402bc6167f30364b3df79f
Mia <mia.sri.susanti.tik21@mhsw.pnj.ac.id>
2023-11-29 19:01:57 +0700

    create filter component

---
+++ src/components/Filter/FilterCourseHome.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line react/prop-types
+const FilterCourseHome = ({title}) => {
+    return (
+        <button className="bg-LIGHTBLUE py-1 px-5 rounded-2xl hover:bg-DARKBLUE05 hover:text-white active:bg-DARKBLUE05 active:text-white">
+            <p className="text-sm">{title}</p>
+        </button>
+    )
+}
+
+export default FilterCourseHome
---
+++ src/components/Filter/FilterPlanProgress.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line react/prop-types
+const FilterPlanProgress = ({title}) => {
+    return (
+        <div>
+            <button className="bg-white text-DARKBLUE06 rounded-full w-56 h-10 font-bold hover:bg-DARKBLUE05 hover:text-white active:bg-DARKBLUE05 active:text-white">{title}</button>
+        </div>
+    )
+}
+
+export default FilterPlanProgress
