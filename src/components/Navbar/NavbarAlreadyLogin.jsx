b8b0b05dd4dd9c2f56596de7586e3a1d1f287275
Mia <mia.sri.susanti.tik21@mhsw.pnj.ac.id>
2023-11-29 19:05:55 +0700

    create navbar component

---
+++ src/components/Navbar/NavbarAlreadyLogin.jsx
@@ -0,0 +1,38 @@
+import logoDummy from "../../assets/img/logo_dummy.png"
+import { Icon } from '@iconify/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faListUl } from '@fortawesome/free-solid-svg-icons'
+
+const NavbarAlreadyLogin = () => {
+    return (
+        <div className="bg-DARKBLUE05 flex px-20 h-[100px]">
+            <img src={logoDummy} className="my-auto w-[125px] h-[30px]" alt="logo belajar" />
+            <div className="my-auto">
+                <div className="bg-white ml-10 w-[526px] h-[62px] rounded-2xl">
+                    <div className="py-3 px-6 flex gap-8">
+                        <input type="text" className="w-[424px] outline-none border-none" placeholder="Cari Kursus Terbaik.." />
+                        <button className="bg-DARKBLUE05 flex items-center justify-center w-[38px] h-[38px] rounded-lg">
+                            <Icon icon="bx:search-alt" color="white" className="w-6 h-6" />
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className='flex gap-4 ml-auto'>
+                <button className='items-center justify-center'>
+                    <div className="bg-DARKBLUE03 flex items-center justify-center gap-2 py-1 px-5 rounded-lg">
+                        <FontAwesomeIcon icon={faListUl} color="white" />
+                        <p className="text-white font-medium">Kelas</p>
+                    </div>
+                </button>
+                <button>
+                    <Icon icon="lucide:bell" color="white" className="w-6 h-6"/>
+                </button>
+                <button>
+                    <Icon icon="lucide:user" color="white" className="w-6 h-6"/>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default NavbarAlreadyLogin
---
+++ src/components/Navbar/NavbarNotLogin.jsx
@@ -0,0 +1,28 @@
+import logoDummy from "../../assets/img/logo_dummy.png"
+import { Icon } from '@iconify/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons'
+
+const NavbarNotLogin = () => {
+    return (
+        <div className="bg-DARKBLUE05 flex px-20 h-[100px]">
+            <img src={logoDummy} className="my-auto w-[125px] h-[30px]" alt="logo belajar" />
+            <div className="my-auto">
+                <div className="bg-white ml-10 w-[526px] h-[62px] rounded-2xl">
+                    <div className="py-3 px-6 flex gap-8">
+                        <input type="text" className="w-[424px] outline-none border-none" placeholder="Cari Kursus Terbaik.." />
+                        <button className="bg-DARKBLUE05 flex items-center justify-center w-[38px] h-[38px] rounded-lg">
+                            <Icon icon="bx:search-alt" color="white" className="w-6 h-6" />
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <button className='flex gap-2 items-center justify-center my-auto ml-auto'>
+                <FontAwesomeIcon icon={faArrowRightToBracket} color="white"/>
+                <p className='text-white font-medium'>Masuk</p>
+            </button>
+        </div>
+    )
+}
+
+export default NavbarNotLogin
