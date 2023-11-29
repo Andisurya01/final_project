50de5b0312388c33561e5af0e1184fc10bb98f8d
Mia <mia.sri.susanti.tik21@mhsw.pnj.ac.id>
2023-11-29 19:01:06 +0700

    create course detail component

---
+++ src/components/CourseDetail/CourseTitle.jsx
@@ -0,0 +1,40 @@
+import { Icon } from '@iconify/react';
+
+// eslint-disable-next-line react/prop-types
+const CourseTitle = ({course, rating, topic, author, level, module, time}) => {
+    return (
+        <div className="px-10 py-3 w-2/3">
+            <div className="flex justify-between">
+                <p className="text-lg font-semibold text-DARKBLUE05">{course}</p>
+                <div className="flex gap-1">
+                    <Icon icon="bi:star-fill" className="text-ATTENTION text-sm"/>
+                    <p className="font-semibold">{rating}</p>
+                </div>
+            </div>
+            <p className="text-lg font-semibold text-left">{topic}</p>
+            <p className="text-sm mt-1 text-left">by {author}</p>
+            <div className="flex justify-between mt-1 w-80">
+                <div className="flex gap-1 items-center">
+                    <Icon icon="mdi:badge-outline" className="mt-1 text-SUCCESS text-lg" />
+                    <p className="text-sm text-DARKBLUE05 font-medium mt-1">{level}</p>
+                </div>
+                <div className="flex gap-1 items-center">
+                    <Icon icon="clarity:book-line" className="mt-1 text-SUCCESS text-lg" />
+                    <p className="text-sm font-medium mt-1">{module}</p>
+                </div>
+                <div className="flex gap-1 items-center">
+                    <Icon icon="ri:time-fill" className="mt-1 text-SUCCESS text-lg" />
+                    <p className="text-sm font-medium mt-1">{time}</p>
+                </div>
+            </div>
+            <div className="bg-SUCCESS text-white flex justify-center items-center px-4 py-1 mt-3 rounded-full w-52">
+                <div className="flex gap-2">
+                    <p className="text-sm font-medium">Join Grup Telegram</p>
+                    <Icon icon="gridicons:chat" className="text-xl" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CourseTitle
