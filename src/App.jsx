e0d76cddc49f4c5c40d5397df0cc1ece07f5695c
Mia <mia.sri.susanti.tik21@mhsw.pnj.ac.id>
2023-11-29 19:17:35 +0700

    adding home & courses router

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,20 @@ import Login from "../src/pages/Login";
 import Register from "../src/pages/Register";
 import ResetTautan from "./pages/ResetTautan";
 import Reset from "./pages/Reset";
+import Home from '../src/pages/Home'
+import CourseTracking from './pages/CourseTracking'
+import Courses from './pages/Courses'
+import CourseDetail from './pages/CourseDetail'
+
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path='/' element={<Home />}> </Route>
+          <Route path='/courseTrackings' element={<CourseTracking />}> </Route>
+          <Route path='/courses' element={<Courses />}> </Route>
+          <Route path='/courses/detail' element={<CourseDetail />}> </Route>
           <Route path="/Login" element={<Login />}></Route>
           <Route path="/Register" element={<Register />}></Route>
           <Route path="/ResetTautan" element={<ResetTautan />}></Route>
