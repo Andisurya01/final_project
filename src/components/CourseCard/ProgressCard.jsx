52f3dc727f23ca66c1d04a16644ef00945ae047d
Mia <mia.sri.susanti.tik21@mhsw.pnj.ac.id>
2023-12-01 13:59:13 +0700

    change button into div

--- src/components/CourseCard/ProgressCard.jsx
+++ src/components/CourseCard/ProgressCard.jsx
@@ -7,7 +7,7 @@ const ProgressCard = ({picture, course, rating, topic, author, level, module, ti
       };
 
     return (
-        <button className="bg-white w-[323px] h-full rounded-2xl" style={{boxShadow: '0px 4px 10px 0px rgba(0, 0, 0, 0.08)'}}>
+        <div className="bg-white w-[323px] h-full rounded-2xl" style={{boxShadow: '0px 4px 10px 0px rgba(0, 0, 0, 0.08)'}}>
             <img src={picture} className="w-full h-full object-cover" style={{width: '323px', height: '80px', borderRadius: '20px 20px 0px 0px'}} alt="" />
             <div className="px-3 py-3">
                 <div className="flex justify-between">
@@ -46,7 +46,7 @@ const ProgressCard = ({picture, course, rating, topic, author, level, module, ti
                     </div>
                 </div>
             </div>
-        </button>
+        </div>
     )
 }
 
