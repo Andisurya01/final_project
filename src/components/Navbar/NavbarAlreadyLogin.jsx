425eb0b64d59834c565d257d0486172ba827b0b7
Mia <mia.sri.susanti.tik21@mhsw.pnj.ac.id>
2023-12-01 14:02:06 +0700

    update menu function

--- src/components/Navbar/NavbarAlreadyLogin.jsx
+++ src/components/Navbar/NavbarAlreadyLogin.jsx
@@ -1,10 +1,48 @@
-import logoDummy from "../../assets/img/logo_dummy.png"
+import { useState, useEffect } from 'react';
 import { Icon } from '@iconify/react';
+import NavbarButton from '../Button/NavbarButton';
+
+import { useNavigate, useLocation } from "react-router-dom"
 
 const NavbarAlreadyLogin = () => {
+    const navigate = useNavigate()
+    const location = useLocation()
+
+    const [isKelasActive, setKelasActive] = useState(true);
+    const [isBellActive, setBellActive] = useState(false);
+    const [isUserActive, setUserActive] = useState(false);
+
+    useEffect(() => {
+        const path = location.pathname;
+        setKelasActive(path === '/courses');
+        setBellActive(path === '/notification');
+        setUserActive(path === '/users');
+    }, [location.pathname]);
+
+    const handleKelasClick = () => {
+        setKelasActive(true);
+        setBellActive(false);
+        setUserActive(false);
+        navigate("/courses")
+    };
+
+    const handleBellClick = () => {
+        setKelasActive(false);
+        setBellActive(true);
+        setUserActive(false);
+        navigate("/notification")
+    };
+
+    const handleUserClick = () => {
+        setKelasActive(false);
+        setBellActive(false);
+        setUserActive(true);
+        navigate("/users")
+    };
+
     return (
         <div className="bg-DARKBLUE05 flex px-20 h-[100px]">
-            <img src={logoDummy} className="my-auto w-[125px] h-[30px]" alt="logo belajar" />
+            <h1 className="my-auto w-[125px] h-[30px] text-3xl text-white font-bold">CraftIQ</h1>
             <div className="my-auto">
                 <div className="bg-white ml-10 w-[526px] h-[62px] rounded-2xl">
                     <div className="py-3 px-6 flex gap-8">
@@ -15,22 +53,14 @@ const NavbarAlreadyLogin = () => {
                     </div>
                 </div>
             </div>
-            <div className='flex gap-4 ml-auto'>
-                <button className='items-center justify-center'>
-                    <div className="bg-DARKBLUE03 flex items-center justify-center gap-2 py-1 px-5 rounded-lg">
-                        <Icon icon="tabler:list" color="white"/>
-                        <p className="text-white font-medium">Kelas</p>
-                    </div>
-                </button>
-                <button>
-                    <Icon icon="lucide:bell" color="white" className="w-6 h-6"/>
-                </button>
-                <button>
-                    <Icon icon="lucide:user" color="white" className="w-6 h-6"/>
-                </button>
+            <div className="flex gap-4 ml-auto">
+                <NavbarButton isActive={isKelasActive} onClick={handleKelasClick} icon="tabler:list" text="Kelas" />
+                <NavbarButton isActive={isBellActive} onClick={handleBellClick} icon="lucide:bell" text="Notifikasi" />
+                <NavbarButton isActive={isUserActive} onClick={handleUserClick} icon="lucide:user" text="Akun" />
             </div>
         </div>
     )
 }
+
 
 export default NavbarAlreadyLogin
