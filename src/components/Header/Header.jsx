42f5779f598f40bebdb5a52a001f6dd1b01e0341
Mia <mia.sri.susanti.tik21@mhsw.pnj.ac.id>
2023-12-13 17:52:01 +0700

    update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,9 @@
+import { useNavigate } from "react-router-dom"
 import cover from "../../assets/img/cover.jpg"
+import AnimatedButton from "../Button/AnimatedButton"
 
 const Header = () => {
+    const navigate = useNavigate()
     return (
         <div className="bg-DARKBLUE05 w-full flex">
             <div className="relative w-[884px] h-[300px]">
@@ -8,11 +11,13 @@ const Header = () => {
                 <div className="absolute top-0 right-0 w-full h-full" style={{background: 'linear-gradient(270deg, #6148FF 5%, rgba(255, 233, 202, 0) 100%)'}}>
                 </div>
             </div>
-            <div className="flex flex-col items-center justify-center">
+            <div className="mx-auto flex flex-col items-center justify-center">
                 <h1 className="text-white text-2xl font-medium">Belajar <br/> dari Praktisi Terbaik!</h1>
-                <button className="bg-white mt-4 flex items-center justify-center w-[250px] h-10 rounded-2xl hover:bg-LIGHTBLUE active:bg-LIGHTBLUE">
-                    <h1 className="text-DARKBLUE05 text-2xl font-bold">IKUTI KELAS</h1>
-                </button>
+                <AnimatedButton>
+                    <button onClick={() => navigate("/courses")} className="bg-white mt-4 flex items-center justify-center w-[250px] h-10 rounded-2xl hover:bg-LIGHTBLUE active:bg-LIGHTBLUE">
+                        <h1 className="text-DARKBLUE05 text-2xl font-bold">IKUTI KELAS</h1>
+                    </button>
+                </AnimatedButton>
             </div>
         </div>
     )
