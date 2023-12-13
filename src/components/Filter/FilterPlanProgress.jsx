8da76e5f3499cc8ecb73c142150fdc780dbb0ecc
Mia <mia.sri.susanti.tik21@mhsw.pnj.ac.id>
2023-12-13 17:48:36 +0700

    update round size

--- src/components/Filter/FilterPlanProgress.jsx
+++ src/components/Filter/FilterPlanProgress.jsx
@@ -2,7 +2,7 @@
 const FilterPlanProgress = ({title}) => {
     return (
         <div>
-            <button className="bg-white text-DARKBLUE06 rounded-full w-56 h-10 font-bold hover:bg-DARKBLUE05 hover:text-white active:bg-DARKBLUE05 active:text-white">{title}</button>
+            <button className="bg-white text-DARKBLUE06 rounded-xl w-56 h-10 font-bold hover:bg-DARKBLUE05 hover:text-white active:bg-DARKBLUE05 active:text-white">{title}</button>
         </div>
     )
 }
