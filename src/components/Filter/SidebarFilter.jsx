0d0afca3b87a47008f2e19a0f6b2ee011ea182ea
Mia <mia.sri.susanti.tik21@mhsw.pnj.ac.id>
2023-12-15 10:57:31 +0700

    add pm & ios filter

--- src/components/Filter/SidebarFilter.jsx
+++ src/components/Filter/SidebarFilter.jsx
@@ -2,8 +2,6 @@ import Checkbox from "../Checkbox/Checkbox";
 
 const SidebarFilter = () => {
 
-    
-
     return (
         <div className="bg-white w-1/4 rounded-xl px-6 py-8">
             <div className="mb-10">
@@ -15,8 +13,10 @@ const SidebarFilter = () => {
             <div className="mb-10">
                 <h1 className="text-2xl font-bold mb-5">Kategori</h1>
                 <Checkbox title={'UI/UX Design'} id={'uiux'} value = {'UI/UX Design'} />
+                <Checkbox title={'Product Management'} id={'pm'} value = {'Product Management'} />
                 <Checkbox title={'Web Development'} id={'webdev'} value = {'Web Development'} />
                 <Checkbox title={'Android Development'} id={'android'} value = {'Android Development'} />
+                <Checkbox title={'iOS Development'} id={'ios'} value = {'iOS Development'} />
                 <Checkbox title={'Data Science'} id={'datasc'} value = {'Data Science'} />
             </div>
             <div className="mb-10">
