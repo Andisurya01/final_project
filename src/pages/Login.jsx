63851f5280d93b9a9160002ad7309ae44482619f
Mia <mia.sri.susanti.tik21@mhsw.pnj.ac.id>
2023-12-15 16:48:04 +0700

    update login responsive

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
         <div className="lg:col-span-7 lg:my-auto lg:px-40 px-10">
           <LoginForm />
         </div>
-        <div className="lg:col-span-5 bg-DARKBLUE05 h-screen lg:grid place-content-center hidden">
+        <div className="hidden col-span-5 bg-DARKBLUE05 h-screen lg:grid place-content-center">
           <div className="flex items-center justify-center">
             <img src={logo} alt="" style={{width: "50%"}} className=""/>
           </div>
