202e70159ad720af0ce663416d8f79e57f68dc34
MuhammadAliffandy <aliffandyrealgo@gmail.com>
2023-12-26 13:18:08 +0700

    update configuration testing

--- test/account.test.js
+++ test/account.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen, fireEvent , waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Account from '../src/pages/Account';
+import '@testing-library/jest-dom'
 
 jest.mock("../src/components/Footer/Footer", () => () => <div data-testid="mocked-footer">Mocked Footer</div>);
 
@@ -48,27 +49,12 @@ jest.mock('../src/api/order', () => ({
 
 describe('Account Component', () => {
     it('renders Account component', async () => {
-        // Mock the image blob for testing
-        global.fetch = jest.fn(() =>
-        Promise.resolve({
-            blob: () => Promise.resolve(new Blob()),
-        })
-        );
 
         render(<Account />);
 
-        // Wait for the asynchronous call to complete using waitFor
-        await waitFor(() => {
-        // Assertions for the expected behavior after the user interaction
-            expect(screen.getByText('Akun')).toBeInTheDocument();
-        });
+        
+        expect(screen.getByText('Kembali ke Beranda')).toBeInTheDocument();
 
-        // Simulate user interactions and test the component's behavior
-        fireEvent.click(screen.getByText('Profil Saya'));
-
-        // Assertions for the expected behavior after the user interaction
-
-        // Clean up the mocks
-        global.fetch.mockClear();
+ 
     });
 });
