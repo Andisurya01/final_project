303dcdf7c17d492d9be6ecccba80f8abe6e0350a
MuhammadAliffandy <aliffandyrealgo@gmail.com>
2023-12-27 12:37:04 +0700

    added config in vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-const PORT = import.meta.env.PORT;
+
+const port = process.env.PORT || 3000;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: PORT || 3000
+    port: port
   }
 })
