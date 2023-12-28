4a8ba79600b2adcd0feb7fd88a2d711c70ed4fc8
Andisurya01 <andi.suryapriyadi@gmail.com>
2023-12-28 15:22:36 +0700

    debugging : redux persist middleware

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,11 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { persistStore, persistReducer } from 'redux-persist'
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer,FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER, } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import moduleCourses from "./moduleCourses"
 
 const persistConfig = {
     key: 'root',
     storage,
+    blacklist: ['updateId', 'resetId', 'register'],
 };
 
 const persistedReducer = persistReducer(persistConfig, moduleCourses);
@@ -14,7 +20,14 @@ const persistedReducer = persistReducer(persistConfig, moduleCourses);
 const store = configureStore({
     reducer: {
         module: persistedReducer,
-    }
+    },
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+
 });
 
 export const persistor = persistStore(store);
