89b12637131de7ab4310c6ec678e14d0a020bb74
Mia <mia.sri.susanti.tik21@mhsw.pnj.ac.id>
2024-01-04 10:52:03 +0700

    update NotFound pages

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -6,11 +6,10 @@ import Footer from "../components/Footer/Footer";
 const NotFound = () => {
   return (
     <section className="">
-      <Header />
         <div className="w-full bg-LIGHTBLUE">
             <div className="grid place-content-center">
                 <div className="w-full lg:w-[1024px] py-5">
-                    <div className="h-60 text-center grid place-content-center">
+                    <div className="h-screen text-center grid place-content-center">
                         <p className="font-bold">Oops! Halaman yang anda cari tidak ditemukan.</p>
                     </div>
                 </div>
